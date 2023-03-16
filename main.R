f219a1324f27b817611899b263aaae1a70fe6688
Rob Griffin <117731652+griffindatasci@users.noreply.github.com>
2023-03-16 13:25:10 +0100

    add figs

--- main.R
+++ main.R
@@ -75,8 +75,8 @@ top10_150 <- crossed[power==150][order(cost_rank)][
 # Plot costs -------------------------------------------------------------------
 crossed[power!=900 & wheel!="Zwift 32mm Carbon" & frame!="Zwift Aero"] %>% 
   ggplot(aes(x=power, y=cost, colour=frame, shape=wheel)) +
-  geom_point(alpha=0.5, size=2) +
-  geom_path() +
+  geom_path(alpha=0.5) +
+  geom_point(size=2) +
   scale_y_reverse("Seconds Behind Fastest Bike") +
   scale_x_continuous("Power",
                      breaks=c(150, 225, 300),
@@ -90,8 +90,8 @@ crossed[power!=900 & wheel!="Zwift 32mm Carbon" & frame!="Zwift Aero"] %>%
 # Plot costs greyed out, focus on best at each power ---------------------------
 crossed[power!=900 & frame!="Zwift Aero" & wheel!="Zwift 32mm Carbon" & best_rank!=1] %>% 
   ggplot(aes(x=power, y=cost, group=paste0(frame, wheel))) +
-  geom_point(colour="#dddddd", alpha=0.5, size=2) +
-  geom_path(colour="#dddddd") +
+  geom_path(colour="#dddddd", alpha=0.5) +
+  geom_point(colour="#dddddd", size=2) +
   scale_y_reverse("Seconds Behind Fastest Bike") +
   scale_x_continuous("Power",
                      breaks=c(150, 225, 300),
@@ -99,18 +99,60 @@ crossed[power!=900 & frame!="Zwift Aero" & wheel!="Zwift 32mm Carbon" & best_ran
   labs(colour="Frame", shape="Wheel") +
   theme_classic() +
   # - add best performers in
+  geom_path(data=crossed[power %in% c(150,300) & frame!="Zwift Aero" & wheel!="Zwift 32mm Carbon" 
+                         & best_rank==1],
+            aes(colour=frame), alpha=0.5) +
   geom_point(data=crossed[power %in% c(150,300) & frame!="Zwift Aero" & wheel!="Zwift 32mm Carbon" 
                           & best_rank==1],
-             aes(colour=frame, shape=wheel), size=2) +
-  geom_path(data=crossed[power %in% c(150,300) & frame!="Zwift Aero" & wheel!="Zwift 32mm Carbon" 
-                          & best_rank==1],
-             aes(colour=frame)) ->
+             aes(colour=frame, shape=wheel), size=2) ->
   crossover_plot_greyed
 
 
 
-top10_300
-top10_150
-crossover_plot
-crossover_plot_greyed
+# Plot frames only -------------------------------------------------------------
+crossed[power!=900 & wheel=="ZIPP 454" & frame!="Zwift Aero",
+        .(frame, wheel, "cost"=cost-min(cost, na.rm=TRUE)), by=power] %>% 
+  ggplot(aes(x=power, y=cost, colour=frame)) +
+  geom_path(alpha=0.5) +
+  geom_point(size=2) +
+  scale_y_reverse("Seconds Behind Fastest Frame") +
+  scale_x_continuous("Power",
+                     breaks=c(150, 225, 300),
+                     labels=c("150W (2W/kg)", "225W (3W/kg)", "300W (4W/kg)")) +
+  labs(colour="Frame") +
+  theme_classic() ->
+  crossover_plot_frames
+
+
+
+# Plot wheels only -------------------------------------------------------------
+crossed[power!=900 & frame=="Scott Addict RC" & wheel!="Zwift 32mm Carbon",
+        .(frame, wheel, "cost"=cost-min(cost)), by=power] %>% 
+  ggplot(aes(x=power, y=cost, colour=wheel)) +
+  geom_path(alpha=0.5) +
+  geom_point(size=2) +
+  scale_y_reverse("Seconds Behind Fastest Wheel") +
+  scale_x_continuous("Power",
+                     breaks=c(150, 225, 300),
+                     labels=c("150W (2W/kg)", "225W (3W/kg)", "300W (4W/kg)")) +
+  labs(colour="Wheel") +
+  theme_classic() ->
+  crossover_plot_wheels
+
+
+
+
+
+
+# Outputs
+# top10_300
+# top10_150
+# crossover_plot
+# ggsave("crossover.png", dpi=600, bg=NA, width=9, height=5)
+# crossover_plot_greyed
+# ggsave("crossover_focus.png", dpi=600, bg=NA, width=9, height=5)
+# crossover_plot_frames
+# ggsave("crossover_frames.png", dpi=600, bg=NA, width=9, height=5)
+# crossover_plot_wheels
+# ggsave("crossover_wheels.png", dpi=600, bg=NA, width=9, height=5)
 
